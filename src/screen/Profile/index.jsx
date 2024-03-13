1c3ff2415b7026bf7a64dd3c2f5ca3a631d349e1
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2024-03-13 15:27:30 -0300

    create profile screen

--- src/screen/Profile/index.jsx
+++ src/screen/Profile/index.jsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import styles from './styles'
 
-export default function Home() {
+export default function Profile() {
   return (
     <View style={styles.container}>
       <Title title="today is Felipe Senai birthday 🧁🎂" />
